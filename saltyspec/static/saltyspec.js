15e1c402f3dcdb88759501180929792c8f09bac0
sahruldotid <sahruldotid@gmail.com>
2022-05-17 22:34:32 +0700

    creating auto preview feautures for user spec tables

--- saltyspec/static/saltyspec.js
+++ saltyspec/static/saltyspec.js
@@ -1,18 +1,32 @@
-function compress(s) {
-	//UTF8
+let compress = (s) => {
 	s = unescape(encodeURIComponent(s));
 	var arr = [];
 	for (var i = 0; i < s.length; i++)
-		arr.push(s.charCodeAt(i));	
+		arr.push(s.charCodeAt(i));
 	var compressor = new Zopfli.RawDeflate(arr);
 	var compressed = compressor.compress();
 	return encode64_(compressed);
 }
 
-function setPreview() {
-    let saltSyntax = document.getElementById('saltSyntax').value;
-    let preview = document.getElementById('saltPreview');
-    // alert(compress(saltSyntax, false));
-    preview.setAttribute('src', 'http://www.plantuml.com/plantuml/png/' + compress(saltSyntax));
-
+let setPreview = () => {
+	let saltSyntax = document.getElementById('saltSyntax').value;
+	let preview = document.getElementById('saltPreview');
+	preview.setAttribute('src', 'http://www.plantuml.com/plantuml/png/' + compress(saltSyntax));
 }
+$(document).ready(() => {
+	$('#actorSource').on('input', () => {
+		$('#actor').html($('#actorSource').val());
+	});
+	$('#descriptionSource').on('input', () => {
+		$('#description').html($('#descriptionSource').val());
+	});
+	$('#initialConditionSource').on('input', () => {
+		$('#initialCondition').html($('#initialConditionSource').val());
+	});
+	$('#finalConditionSource').on('input', () => {
+		$('#finalCondition').html($('#finalConditionSource').val());
+	});
+	$('#saltSyntax').on('input', () => {
+		setPreview();
+	});
+});
