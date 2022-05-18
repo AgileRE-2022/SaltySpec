ad59a89df15f3d54fdfa68f7fcc78a6c7d130cfe
sahruldotid <sahruldotid@gmail.com>
2022-05-18 14:27:56 +0700

    Implementing regex in order to grab some important string from salt syntax

--- saltyspec/static/saltyspec.js
+++ saltyspec/static/saltyspec.js
@@ -9,10 +9,54 @@ let compress = (s) => {
 }
 
 let setPreview = () => {
-	let saltSyntax = document.getElementById('saltSyntax').value;
-	let preview = document.getElementById('saltPreview');
-	preview.setAttribute('src', 'http://www.plantuml.com/plantuml/png/' + compress(saltSyntax));
+	let saltSyntax = $('#saltSyntax').val();
+	$('#saltPreview').attr('src', 'http://www.plantuml.com/plantuml/png/' + compress(saltSyntax))
 }
+
+let delay = (function () {
+	let timer = 0;
+	return function (callback, ms) {
+		clearTimeout(timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+let setUseSpec = function () {
+	let syntax = $('#saltSyntax').val();
+
+	// Title
+	let titleRegex = /title\s(.*)\n/g;
+	let matchTitle = titleRegex.exec(syntax);
+	if (matchTitle != null) {
+		$('#useCaseName').html(matchTitle[1]);
+	}
+
+	// Procedure
+	let procedureForm = /!procedure _form()([\S\s]*?)!endprocedure/g;
+	let matchProcedure = procedureForm.exec(syntax);
+	if (matchProcedure != null) {
+		formString = matchProcedure[0]
+		formString = formString.replace('!procedure _form()\n', "").replace('!endprocedure', "")
+		alert(formString)
+	}
+
+	let procedureSuccess = /!procedure _success()([\S\s]*?)!endprocedure/g;
+	let matchSuccess = procedureSuccess.exec(syntax);
+	if (matchSuccess != null) {
+		successString = matchSuccess[0]
+		successString = successString.replace('!procedure _success()\n', "").replace('!endprocedure', "")
+		alert(successString)
+	}
+
+	let procedureError = /!procedure _error()([\S\s]*?)!endprocedure/g;
+	let matchError = procedureError.exec(syntax);
+	if (matchError != null) {
+		errorString = matchError[0]
+		errorString = errorString.replace('!procedure _error()\n', "").replace('!endprocedure', "")
+		alert(errorString)
+	}
+}
+
 $(document).ready(() => {
 	$('#actorSource').on('input', () => {
 		$('#actor').html($('#actorSource').val());
@@ -26,7 +70,10 @@ $(document).ready(() => {
 	$('#finalConditionSource').on('input', () => {
 		$('#finalCondition').html($('#finalConditionSource').val());
 	});
-	$('#saltSyntax').on('input', () => {
-		setPreview();
+	$('#saltSyntax').on('keyup', () => {
+		delay(function () {
+			setPreview();
+			setUseSpec();
+		}, 1000);
 	});
 });
